45c5d39df6f3891e596b459a9bf34330465cf5df
JonMen7 <jonathanmen566@gmail.com>
2023-03-23 01:27:19 -0700

    Task 2 Multiply two integers

---
+++ 2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiply 2 integers
+ * @a: first inte
+ * @b: second int
+ *
+ * Return: result of multiplication sum
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
